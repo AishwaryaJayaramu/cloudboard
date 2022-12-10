1577c4983a2e65e00b5180ccdf6a2df53cb6b5ac
Aishwarya Jayaramu <aija7428@colorado.edu>
2022-12-10 15:07:17 -0700

    add device registration

--- cloudboard/server/CloudBoardServer.py
+++ cloudboard/server/CloudBoardServer.py
@@ -1,14 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
+from .models import Device
+import jwt
+import hashlib
 from . import db
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
+
+@main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@main.route('/profile')
+
+@main.route("/profile")
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    return render_template(
+        "profile.html",
+        name=current_user.name,
+        current_devices=list(map(lambda x: x.name, current_user.devices)),
+    )
+
+
+@main.route("/add_device", methods=["POST"])
+@login_required
+def add_device():
+    device_name = request.form.get("device")
+    if device_name:
+        
+        jwt_token = jwt.encode({"name": device_name}, "secret", algorithm="HS256")
+        encoded_jwt = hashlib.sha256(jwt_token.encode('utf-8')).hexdigest()
+        device = Device(name=device_name, token_hash = encoded_jwt, user_id=current_user.id)
+        db.session.add(device)
+        db.session.commit()
+        flash(f"Device {device_name} added. Please copy the token {jwt_token} and store it in a safe place.")
+
+    return render_template(
+        "profile.html", 
+        name=current_user.name
+    )
--- cloudboard/server/models.py
+++ cloudboard/server/models.py
@@ -1,9 +1,19 @@
 from flask_login import UserMixin
 from . import db
 
+
 class User(UserMixin, db.Model):
     # primary keys are required by SQLAlchemy
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    devices = db.relationship("Device", backref="post")
+
+
+class Device(db.Model):
+    # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    token_hash = db.Column(db.String(1000))
     name = db.Column(db.String(1000))
